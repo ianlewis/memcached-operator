fbd953c9d95a37401df09847319647151f04f6e5
Ian Lewis <ianlewis@google.com>
2017-12-11 13:35:06 +0900

    Added comment

--- pkg/controller/proxy/controller_test.go
+++ pkg/controller/proxy/controller_test.go
@@ -1,17 +1,3 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package proxy
 
 import (
@@ -158,6 +144,7 @@ func (f *fixture) run_(key string, expectError bool) {
 	}
 }
 
+// TestNewProxy tests the handling of the memcached proxy controller when a new memcached proxy is created
 func TestNewProxy(t *testing.T) {
 	f := newFixture(t)
 

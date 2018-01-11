2bf5b919680ef9c3b1797eac524a4a2291e204ad
Ian Lewis <ianlewis@google.com>
2018-01-11 22:22:41 +0900

    Added tests for routes with children

--- pkg/controller/proxyconfigmap/config_test.go
+++ pkg/controller/proxyconfigmap/config_test.go
@@ -33,6 +33,43 @@ func TestRouteForRule(t *testing.T) {
 
 	})
 
+	t.Run("sharded rule with children should be created correctly", func(t *testing.T) {
+		r := v1alpha1.RuleSpec{
+			Type: v1alpha1.ShardedRuleType,
+			Children: []v1alpha1.RuleSpec{
+				{
+					Type: v1alpha1.ShardedRuleType,
+					Service: &v1alpha1.ServiceSpec{
+						Name:      "foo",
+						Namespace: "default",
+						Port:      intstr.IntOrString{IntVal: 11211},
+					},
+				},
+				{
+					Type: v1alpha1.ShardedRuleType,
+					Service: &v1alpha1.ServiceSpec{
+						Name:      "bar",
+						Namespace: "default",
+						Port:      intstr.IntOrString{IntVal: 11211},
+					},
+				},
+			},
+		}
+
+		services := make(map[string]*v1alpha1.ServiceSpec)
+		route, _, err := f.controller.routeForRule(r, services)
+		if !assert.NoError(t, err, "must be able to generate routes") {
+			assert.FailNow(t, "failing early")
+		}
+
+		assert.Equal(t, route.Route.Type, "HashRoute")
+		if !assert.Len(t, route.Route.Children, 2) {
+			assert.FailNow(t, "failing early")
+		}
+		assert.Equal(t, route.Route.Children[0].StringVal, "PoolRoute|"+poolNameForServiceSpec(r.Children[0].Service))
+		assert.Equal(t, route.Route.Children[1].StringVal, "PoolRoute|"+poolNameForServiceSpec(r.Children[1].Service))
+	})
+
 	t.Run("replicated rule with service should be created correctly", func(t *testing.T) {
 		r := v1alpha1.RuleSpec{
 			Type: v1alpha1.ReplicatedRuleType,
@@ -52,5 +89,42 @@ func TestRouteForRule(t *testing.T) {
 		assert.Equal(t, route.Route.Type, "OperationSelectorRoute")
 		assert.Equal(t, route.Route.DefaultPolicy.StringVal, "AllSyncRoute|Pool|"+poolNameForServiceSpec(r.Service))
 		assert.Equal(t, route.Route.OperationPolicies.Delete.StringVal, "LatestRoute|Pool|"+poolNameForServiceSpec(r.Service))
+	})
+
+	t.Run("replicated rule with children should be created correctly", func(t *testing.T) {
+		r := v1alpha1.RuleSpec{
+			Type: v1alpha1.ReplicatedRuleType,
+			Children: []v1alpha1.RuleSpec{
+				{
+					Type: v1alpha1.ShardedRuleType,
+					Service: &v1alpha1.ServiceSpec{
+						Name:      "foo",
+						Namespace: "default",
+						Port:      intstr.IntOrString{IntVal: 11211},
+					},
+				},
+				{
+					Type: v1alpha1.ShardedRuleType,
+					Service: &v1alpha1.ServiceSpec{
+						Name:      "bar",
+						Namespace: "default",
+						Port:      intstr.IntOrString{IntVal: 11211},
+					},
+				},
+			},
+		}
+
+		services := make(map[string]*v1alpha1.ServiceSpec)
+		route, _, err := f.controller.routeForRule(r, services)
+		if !assert.NoError(t, err, "must be able to generate routes") {
+			assert.FailNow(t, "failing early")
+		}
+
+		assert.Equal(t, route.Route.Type, "OperationSelectorRoute")
+		assert.Equal(t, route.Route.DefaultPolicy.Route.Type, "AllSyncRoute")
+		assert.Len(t, route.Route.DefaultPolicy.Route.Children, 2)
+
+		assert.Equal(t, route.Route.OperationPolicies.Delete.Route.Type, "LatestRoute")
+		assert.Len(t, route.Route.OperationPolicies.Delete.Route.Children, 2)
 	})
 }
